42c14958d472575fd43f7048aaa30e9b7b10675a
Szczudlo-Magdalena <56344130+Szczudlo-Magdalena@users.noreply.github.com>
2020-07-12 08:33:52 +0200

    lab4,5

---
+++ wsei-xamarin-lab5/AirMonitor/AirMonitor/Views/MapPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using AirMonitor.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        private HomeViewModel _viewModel => BindingContext as HomeViewModel;
+
+        public MapPage()
+        {
+            InitializeComponent();
+            BindingContext = new HomeViewModel(Navigation);
+        }
+
+        private void Pin_InfoWindowClicked(object sender, Xamarin.Forms.Maps.PinClickedEventArgs e)
+        {
+            _viewModel.InfoWindowClickedCommand.Execute(((Xamarin.Forms.Maps.Pin)sender).Address);
+        }
+    }
+}
